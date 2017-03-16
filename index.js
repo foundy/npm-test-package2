36c9c35e8e711b5b5ab03fa902355c7ed865440f
Foundy <foundy@foundy.io>
2017-03-16 12:28:45 +0900

    Add common package

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 'use strict';
 
+var common = require('workflow-test-common');
+
 module.exports = function () {
   var span = document.createElement('span');
   span.setAttribute('style', 'color:red');
   span.textContent = 'Component : npm-test-pakcage2';
+  span.textContent += common(span.textContent);
 
   var div = document.createElement('div');
   div.appendChild(span);
